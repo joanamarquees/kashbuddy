3acdf9ad42b0131b1ff5599d10d3ee15843c6612
joanamarquees <joaninhacfmarques@gmail.com>
2024-10-12 07:36:35 +0100

    corrected some logics in accounts page and authentication page

--- src/pages/accounts/index.jsx
+++ src/pages/accounts/index.jsx
@@ -1,38 +1,47 @@
 import { IoMdArrowRoundBack } from "react-icons/io";
 import { MdAddCard } from "react-icons/md";
-import { Button } from '../../components/button.tsx';
-import { Popup } from '../../components/popup.tsx';
-import { setDrawerState, Drawer } from '../../components/drawer.tsx';
-import { NewAccountForms } from '../../components/new-account.tsx';
+import { useNavigate } from 'react-router-dom';
+
+import { Button } from '../../components/Button.jsx';
+import { Popup } from '../../components/Popup.jsx';
+import { setDrawerState, Drawer } from '../../components/Drawer.jsx';
+import { NewAccountForms } from '../../components/NewAccount.jsx';
 
 import { useGetAccounts } from '../../hooks/useGetAccounts.js';
 
 export function Accounts() {
+  const navigate = useNavigate();
   
   const { accounts } = useGetAccounts();
-
   const totalNetWorth = accounts.reduce((acc, account) => acc + parseFloat(account.amount), 0);
-  
+
   return (
     <div className="container mx-auto px-4 h-full">
       <Drawer views={{"New-account": <NewAccountForms/>}}/>
       {/* Header */}
-      <div className="py-10 flex flex-row items-center justify-center gap-3 space-x-20">
-        <IoMdArrowRoundBack size={30} />
-
-        <h1 className="text-2xl md:text-4xl font-bold font-sans"> Accounts </h1>
-
+      <div className="py-6 flex flex-row items-center justify-center gap-6">
         {accounts.length >= 1 ?
           <button> 
-            <MdAddCard size={30} onClick={() => setDrawerState("New-account")}/> {/** () => navigate('/new-account') */}
+            <MdAddCard
+              size={30}
+              onClick={() => setDrawerState("New-account")}
+              className='cursor-pointer mx-2'
+            />
           </button>
           : <MdAddCard size={30} className="invisible" />
         }
+
+        <h1 className="text-2xl md:text-4xl font-bold font-sans mx-2"> BankAccounts </h1>
+
+        <IoMdArrowRoundBack
+          size={30}
+          className='cursor-pointer mx-2 translate rotate-180'
+          onClick={() => navigate('/home')}
+        />
       </div>
-      
-      {/* Empty accounts page */}
+
       {accounts.length <= 0 ? (
-        <div className="py-60 flex flex-col items-center justify-center gap-8 md:text-lg">
+        <div className="py-6 flex flex-col items-center justify-center gap-8 md:text-lg">
           <p className="text-zinc-300 leading-relaxed max-w-80 md:max-w-lg text-center font-sans">
             You haven't registered any bank account, how about registering one right now?
           </p>
@@ -46,14 +55,14 @@ export function Accounts() {
         // Accounts page list
         <div>
           <h2 className="text-zinc-400 text-center text-2xl md:text-4xl font-sans"> NET WORTH </h2>
-          <h3 className="text-pink-500 text-center font-extrabold text-5xl md:text-7xl py-10 font-sans"> {totalNetWorth}€ </h3>
+          <h3 className="text-indigo-400 text-center font-extrabold text-5xl md:text-7xl py-10 font-sans"> {totalNetWorth}€ </h3>
           <div className="flex flex-col gap-3">
             {accounts.map(({bankName, amount}) => (
               <Popup key={bankName} bankName={bankName} amount={amount}/> 
             ))}
           </div>
         </div>
-      )}    
+      )}
     </div>
   )
 }
--- src/pages/auth/index.jsx
+++ src/pages/auth/index.jsx
@@ -1,37 +1,54 @@
-import { auth, provider } from '../../config/firebase-config';
-import { signInWithPopup } from 'firebase/auth';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom'; // to change from login to home page
-import { Button } from '../../components/button.tsx';
+
+import { auth, provider } from '../../config/firebase-config';
+import { signInWithRedirect } from 'firebase/auth';
+
+import { Button } from '../../components/Button.jsx';
 
 import logo from '../../assets/logo_light.svg'
 
 export const Auth = () => {
-
   const navigate = useNavigate();
-
+  const [error, setError] = useState(null);
+    
   const signInWithGoogle = async () => {
     // Sign in with Google
-    const results = await signInWithPopup(auth, provider); // async call
+    try {
+      var results = await signInWithRedirect(auth, provider); // async call
+    } catch (error) {
+      setError(error.message);
+      return;
+    }
+    
     const authInfo = {
       userId: results.user.uid,
       name: results.user.displayName,
       isAuth: true, // to check that the user is logged in
     }
     localStorage.setItem('auth', JSON.stringify(authInfo)); // because we can't store objects in local storage
-    navigate('/home'); // redirect to home page
+    
+    navigate('/home');
   };
 
   return (
     <div className="h-screen flex flex-col items-center justify-center gap-8">
-      <img src={logo} alt="Kashbuddy logo"/>
-      <p className="leading-relaxed max-w-80 text-center font-sans font-semibold text-lg text-green-100"> 
+      <img src={logo} className="w-64 md:w-96" alt="Kashbuddy logo"/>
+      <div className="leading-relaxed max-w-80 text-center font-sans font-semibold text-lg text-green-100"> 
         <p> Welcome to Kashbuddy! </p>
         <p> Please login with Google to continue </p>
-      </p>
-      <Button onClick={signInWithGoogle}>
+      </div>
+      <Button className="loetError(error.message);gin-button" onClick={signInWithGoogle}>
         Login with Google
       </Button>
-      {/* <button className="login-button" onClick={signInWithGoogle}> Login with Google </button> */}
+      <p className="text-indigo-400 text-sm">{error}</p>
+
+      {/* TODO: once logged in, if the user has no accounts, then redirect to accounts page */}
+      {/* {accounts.length >= 1 ?
+        navigate('/home') // redirect to accounts page
+        : navigate('/accounts') // redirect to accounts page
+      } */}
+
     </div>
   );
 };
